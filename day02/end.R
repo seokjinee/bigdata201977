a855bceff479a088c23965ac321ae32868306711
seokjinee <47075017+seokjinee@users.noreply.github.com>
2019-02-10 17:43:15 +0900

    Add files via upload

--- day02/end.R
+++ day02/end.R
@@ -59,3 +59,56 @@ if(hour(date)<11){
 }else{
   print("굿나잇")
 }
+
+date + minutes(10) # date에 10분을 더한 날짜와 시각
+date
+
+########################################################
+# 입력받은 값 판별
+input <- scan(what = "") #scan에 문자값을 입력받을 때는 what = "" 넣기
+if(input == "100점"){
+  print("만점입니다.")
+} else{
+  print("다음 기회에")
+}
+
+# 삼항 연산자
+temp3 <- 80
+result <- ifelse(temp3 >= 80,"통과","실패")
+result
+
+########################################################
+temp4 <- 10 #무한루프 중단 단축키: 컨트롤 씨
+while(temp4 > 8){
+  print("7보다 크다")
+  temp4 <- temp4-1
+}
+for(i in c(1:5)){
+  print("%")
+}
+temp5 <- c(1:5)
+for(temp6 in temp5){
+  print(temp6)
+}
+
+name <- c("김아무개","박아무개","송아무개","정아무개")
+for(temp8 in name){
+  cat("당신의 이름은", temp8,  "이군요\n")  # 역슬러시 n
+}
+
+student <- data.frame(name2 = name, age2 = c(30,50,15,40))
+student
+
+for(temp9 in student$name2){
+  print(temp9)
+}
+
+input2 <- scan()
+while(TRUE){
+  input2 <- scan()
+  print(input2)
+  if(input2==0){
+    print("종료합니다")
+    break
+  }
+}
